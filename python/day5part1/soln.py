5e448a7363f5ee71e7a5836eaa600f948835d618
Elliot <elliot@bitoffdev.com>
2021-12-05 16:08:04 -0500

    Day 5 Python solutions

---
+++ python/day5part1/soln.py
@@ -0,0 +1,139 @@
+from dataclasses import dataclass
+import decimal
+from itertools import combinations
+import math
+from typing import Optional
+
+
+# exact_ctx = decimal.Context(traps=[decimal.Inexact])
+# with decimal.localcontext(ctx):
+#     result = decimal.Decimal('1') / decimal.Decimal('3')
+
+@dataclass
+class Point:
+    x: decimal.Decimal
+    y: decimal.Decimal
+    @classmethod
+    def from_ints(cls, x, y):
+        return cls(x=decimal.Decimal(x), y=decimal.Decimal(y))
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+
+@dataclass
+class Line:
+    """
+    Ax + By + C = 0
+    """
+    A: decimal.Decimal
+    B: decimal.Decimal
+    C: decimal.Decimal
+    minX: decimal.Decimal
+    minY: decimal.Decimal
+    maxX: decimal.Decimal
+    maxY: decimal.Decimal
+    
+    @classmethod
+    def from_points(cls, p1, p2):
+        if p2.x == p1.x:
+            A = decimal.Decimal('1')
+            B = decimal.Decimal('0')
+            C = -A * p1.x
+        else:
+            m = (p2.y - p1.y) / (p2.x - p1.x)
+            A = m
+            B = decimal.Decimal('-1')
+            C = -A * p1.x -B * p1.y
+        return cls(A=A,B=B,C=C,minX=min(p1.x,p2.x),minY=min(p1.y,p2.y),maxX=max(p1.x,p2.x),maxY=max(p1.y,p2.y))
+
+    def intersect(self, other) -> Optional[Point]:
+        """
+        Raises:
+            Infinite
+        """
+        # Bail out for parallel lines
+        if self.B * other.A == self.A * other.B:
+            return []
+        
+        # March over the lines
+        if self.x < other.x:
+            p1 = self
+            p2 = other
+        else:
+            p1 = other
+            p2 = self
+        import math
+        for x in range(math.ceil(p1.x), math.floor(p2.x) + 1):
+            y = (-self.A * x - self.C) / self.B
+
+        try:
+            y = (self.A * other.C - self.C * other.A) / (self.B * other.A - self.A * other.B)
+        except decimal.InvalidOperation as exc:
+            print(exc)
+            print(dir(exc))
+            print(exc.args[0])
+            print(exc.args[0] == decimal.DivisionUndefined)
+            print(exc.args[0] == decimal.DivisionByZero)
+            raise Exception("Infinitely many intersections")
+        except decimal.DivisionByZero:
+            # Zero intersections, ie. parallel lines
+            return None
+        
+        if self.A == 0:
+            x = -(other.B * y + self.C) / other.A
+        else:
+            x = -(self.B * y + self.C) / self.A
+            
+        # Check if the intersection is within the segment
+        if min(p1.x, p2.x) <= x <= max(p1.x, p2.x) and min(p1.y, p2.y) <= y <= max(p1.y, p2.y):
+            return Point(x, y)
+        else:
+            return None
+
+
+def march(self):
+    points = set()
+    # March along the X axis
+    if self.B != 0:
+        for x in range(math.ceil(self.minX), math.floor(self.maxX) + 1):
+            y = (-self.A * x - self.C) / self.B
+            points.add(Point(x=x,y=y))
+    elif self.A != 0:
+        for y in range(math.ceil(self.minY), math.floor(self.maxY) + 1):
+            x = (-self.B * y - self.C) / self.A
+            points.add(Point(x=x,y=y))
+    else:
+        raise Exception("Unexpected state")
+    return points
+
+def parse(text_line) -> Line:
+    text_p1, text_p2 = text_line.split('->')
+    p1 = Point.from_ints(*map(lambda s: int(s.strip()), text_p1.split(',')))
+    p2 = Point.from_ints(*map(lambda s: int(s.strip()), text_p2.split(',')))
+    return Line.from_points(p1, p2)
+
+
+
+
+
+
+
+
+import sys
+fh = sys.stdin
+
+
+lines = [
+    parse(l) for l in fh
+]
+# Filter out diagonal lines
+lines = [
+    line for line in lines
+    if line.A == 0 or line.B == 0
+]
+intersections = set()
+for l1, l2 in combinations(lines, 2):
+    inter = march(l1) & march(l2)
+    intersections.update(inter)
+print(len(intersections))
+
---
+++ python/day5part2/README.md
@@ -0,0 +1,4 @@
+This current solution takes around 2-3 minutes. The original idea was to calculate the intersections exactly pairwise.
+
+Possible fixes:
+- Calculate intersection. Only when infinite solutions are detected should a march / walk be required.
